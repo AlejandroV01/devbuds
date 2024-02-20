2b6be890e380ba64577357c34a3703d650555fa7
Alejandro Vera <alexvera0109@gmail.com>
2024-02-20 01:55:21 -0500

    reverted a change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,9 @@
 function App() {
   return (
     <div className='flex items-center justify-center mt-20'>
-      <div className='relative p-[2px] bg-white -z-1 rounded-[4px]'>
-        <div className=' bg-blue-500  w-100 p-2 relative  rounded-[4px]'>
-          <div className='w-1/2 flex flex-col gap-2'>
-            <h1 className='text-white font-bold'>My React and TypeScript App</h1>
-            <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quam?</p>
-          </div>
-        </div>
-        <div className='absolute bg-gradient-to-r from-transparent to-blue-950 w-1/2 h-full top-0 right-0'></div>
-      </div>
+      <h1>App</h1>
     </div>
   )
 }
 
 export default App
-
-const FadedBox = () => {
-  return (
-    <div className='flex items-center justify-center mt-20'>
-      <div className='relative p-[2px] bg-white -z-1 rounded-[4px]'>
-        <div className=' bg-blue-500  w-100 p-2 relative  rounded-[4px]'>
-          <div className='w-1/2 flex flex-col gap-2'>
-            <h1 className='text-white font-bold'>My React and TypeScript App</h1>
-            <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quam?</p>
-          </div>
-        </div>
-        <div className='absolute bg-gradient-to-r from-transparent to-blue-950 w-1/2 h-full top-0 right-0'></div>
-      </div>
-    </div>
-  )
-}
