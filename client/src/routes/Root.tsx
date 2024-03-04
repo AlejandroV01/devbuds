4dfaf539e95be49a4dcd68e9dba69d203d37e61f
Alejandro Vera <alexvera0109@gmail.com>
2024-03-03 21:19:16 -0500

    more bioler plate addition of routing and dark mode

---
+++ client/src/routes/Root.tsx
@@ -0,0 +1,13 @@
+import Nav from '@/components/Nav'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+const Root = () => {
+  return (
+    <main className='flex flex-col'>
+      <Nav />
+      <Outlet />
+    </main>
+  )
+}
+
+export default Root
