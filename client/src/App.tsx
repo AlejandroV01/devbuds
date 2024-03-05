8503d33eb7371a0d636a5b1b3ed679182d377ac2
Alejandro Vera <alexvera0109@gmail.com>
2024-03-05 15:46:32 -0500

    change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,12 @@
+import Button from './components/Button'
 function App() {
   return (
-    <div className='flex items-center justify-center mt-20'>
-      <div className='relative p-[2px] bg-white -z-1 rounded-[4px]'>
-        <div className=' bg-blue-500  w-100 p-2 relative  rounded-[4px]'>
-          <div className='w-1/2 flex flex-col gap-2'>
-            <h1 className='text-white font-bold'>My React and TypeScript App</h1>
-            <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quam?</p>
-          </div>
-        </div>
-        <div className='absolute bg-gradient-to-r from-transparent to-blue-950 w-1/2 h-full top-0 right-0'></div>
-      </div>
+    <div className=''>
+      <h1>App</h1>
+      <Button />
+      hu
     </div>
   )
 }
 
 export default App
-
-const FadedBox = () => {
-  return (
-    <div className='flex items-center justify-center mt-20'>
-      <div className='relative p-[2px] bg-white -z-1 rounded-[4px]'>
-        <div className=' bg-blue-500  w-100 p-2 relative  rounded-[4px]'>
-          <div className='w-1/2 flex flex-col gap-2'>
-            <h1 className='text-white font-bold'>My React and TypeScript App</h1>
-            <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quam?</p>
-          </div>
-        </div>
-        <div className='absolute bg-gradient-to-r from-transparent to-blue-950 w-1/2 h-full top-0 right-0'></div>
-      </div>
-    </div>
-  )
-}
