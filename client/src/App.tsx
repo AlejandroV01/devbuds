cf53337ee035b035f9df2fe9bb372f160dcba838
Alejandro Vera <alexvera0109@gmail.com>
2024-03-06 16:16:52 -0500

    fixed some stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,7 @@
-import Button from './components/Button'
 function App() {
   return (
-<<<<<<< HEAD
     <div className=''>
       <h1>App</h1>
-      <Button />
-      hu
-=======
-    <div className='flex items-center justify-center mt-20'>
-      <h1>App</h1>
->>>>>>> 9dd9f8050fec7123ccfdb3bd989646c0db5fc1de
     </div>
   )
 }
