e7767da7806ff50b9d828cbaa3095f225de3c429
Alejandro Vera <alexvera0109@gmail.com>
2024-03-08 16:33:30 -0500

    added supabase signup, signin, and signout basic functions

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -7,25 +7,34 @@ import { ModeToggle } from './mode-toggle'
 const Nav = () => {
   const [session, setSession] = useState<Session | null>(null)
   useEffect(() => {
+    console.log(session)
+    handleSessionCheck()
+  }, [session])
+  const handleSessionCheck = () => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
     })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  const handleSignIn = async (email: string, password: string) => {
+  }
+  const handleSignUp = async (email: string, password: string) => {
     const { user, session, error } = await supabase.auth.signUp({
       email,
       password,
     })
+    handleSessionCheck()
     console.log(user, session, error)
+  }
+  const handleSignIn = async (email: string, password: string) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    handleSessionCheck()
+    console.log(data, error)
+  }
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    handleSessionCheck()
+    console.log(error)
   }
   return (
     <div>
@@ -37,8 +46,14 @@ const Nav = () => {
         <button></button>
         <button></button>
         <p>{!session ? 'NOT SIGNED IN' : 'SIGNED IN'}</p>
-        <button className='p-2 bg-sky-500' onClick={() => handleSignIn('test@test.com', '1234567')}>
+        <button className='p-2 bg-sky-500' onClick={() => handleSignUp('alexvera0109@gmail.com', '1234567')}>
+          Mock Sign Up
+        </button>
+        <button className='p-2 bg-sky-500' onClick={() => handleSignIn('alexvera0109@gmail.com', '1234567')}>
           Mock Sign In
+        </button>
+        <button className='p-2 bg-destructive' onClick={handleSignOut}>
+          Sign Out
         </button>
       </div>
       <ModeToggle />
