2373c20aab7f4317fb08d0149278bef7e9ded253
dab1an <dabianextras@gmail.com>
2024-03-08 15:52:42 -0500

    Navbar update, setup for redux

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { ModeToggle } from './mode-toggle'
+import React from "react";
+import { ModeToggle } from "./mode-toggle";
+import { IoSearch } from "react-icons/io5";
+
 const Nav = () => {
   return (
     <div>
-      <h1>Nav</h1>
+      <div className=" w-full  h-[25px] bg-primary flex items-center justify justify-between px-[2%] text-white">
+        <div className="font-bold flex items-center text-lg">devBuds</div>
+        <div>
+          <IoSearch color="" />
+        </div>
+        <button></button>
+        <button></button>
+      </div>
       <ModeToggle />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
