f6babf602778e446964ae73d132bc684a06c8d87
Alejandro Vera <alexvera0109@gmail.com>
2024-03-15 18:48:58 -0400

    some base code

--- client/src/components/SkillCard.tsx
+++ client/src/components/SkillCard.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-
-const SkillCard = () => {
+import React, { useState } from 'react'
+import Button from './Button'
+import Badge from './badge'
+const SkillCard = ({ onFinishEditing }: { onFinishEditing: () => void }) => {
+  const arr = ['mike', 'ben', 'jeff']
+  const makeHello = () => {
+    console.log('hello')
+  }
+  const [isEdit, setIsEdit] = useState<boolean>(false)
   return (
     <div>
       <h1>SkillCard</h1>
       <p>Test</p>
+      {arr.map(name => {
+        return <Badge label={name} />
+      })}
+      <Button onClick={onFinishEditing} variant='primary'>
+        Done
+      </Button>
+      {!isEdit ? <p onClick={() => setIsEdit(true)}>Pencil</p> : <p>No Pencil</p>}
     </div>
   )
 }
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 //changed Badge file name
+import SkillCard from '@/components/SkillCard'
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <SkillCard />
     </div>
   )
 }
