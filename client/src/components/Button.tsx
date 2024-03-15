8eb2801603932873a38165d3c40d836ba3221b01
duranmichael681 <duranmichael681@gmail.com>
2024-03-14 22:26:31 -0400

    Hey Dude just got done :)

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+interface ButtonProps {
+  variant: 'primary' | 'secondary' | 'success' | 'destructive'
+  disabled?: boolean
+  onClick?: () => void
+}
 
-const Button = () => {
-  return <button className='bg-red-500 p-2'>Button</button>
+const Button = ({ variant, disabled = false, onClick }: ButtonProps) => {
+  const checkButtonVariance = () => {
+    switch (variant) {
+      case 'primary':
+        return 'bg-primary'
+      case 'secondary':
+        return 'bg-secondary'
+      case 'destructive':
+        return 'bg-destructive'
+    }
+  }
+
+  return (
+    <button disabled={disabled} className={`${checkButtonVariance()} px-3 py-1.5 text-white rounded font-medium w-fit`} onClick={onClick}>
+      text
+    </button>
+  )
 }
 
 export default Button
