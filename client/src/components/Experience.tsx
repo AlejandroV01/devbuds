0a14f976519c4a334326ee3a33102c3cff0dfc33
dab1an <dabianextras@gmail.com>
2024-03-15 23:59:27 -0400

    mockup experience

---
+++ client/src/components/Experience.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="flex bg-[#020817]  h-screen w-[100%]">
+      <div className="bg-dark h-full w-[20%]"></div>
+      <div className="bg-[#020817] h-full w-[80%] flex justify-center">
+        <div className="bg-gray-200 h-full w-[800px] flex flex-col gap-2 p-[3%] rounded-[8px]">
+          <div className="bg-black h-[20%] w-full"></div>
+          <div className=" w-full flex flex-col p-1">
+            <div className="flex gap-4 h-[80px] mb-4 text-black">
+              <img
+                src="https://media.licdn.com/dms/image/D4E0BAQHcyC1UQRfsWQ/company-logo_200_200/0/1707366094747/initofficial_logo?e=2147483647&v=beta&t=7y8ERn_Rj7oXfdxCkujsEj5L2dEe30EBx7ex2Mt5eYs"
+                alt=""
+                className="h-full rounded-[8px] drop-shad"
+              />
+              <div className="flex flex-col h-full">
+                <span className="font-extrabold text-lg text-2">
+                  Software Engineering Intern
+                </span>
+                <span className="mb-[5px]">Blackstone Inc</span>
+
+                <span className="text-gray-500 text-sm">
+                  May 2024 - August 2024
+                </span>
+              </div>
+            </div>
+            <div>
+              <p className="text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+                enim cupiditate odio accusantium voluptatum nisi perspiciatis,
+                animi sint maiores laboriosam tempore quaerat a provident omnis
+                iste, inventore sed. Dicta animi eveniet dolores a recusandae
+                doloribus nostrum magni provident omnis cupiditate placeat modi
+                facere laboriosam dolor, necessitatibus eaque reiciendis
+                pariatur commodi nobis, perspiciatis sunt totam iure dolorum
+                voluptatibus? Voluptates enim quaerat maiores? Sit sunt, quidem
+                quasi magnam tempora perferendis voluptatum minima ad assumenda,
+                quis unde modi iste ratione nulla. Ut ad tempora quos aliquid
+                recusandae, accusamus id placeat blanditiis sed quo voluptate
+                provident voluptatem repellendus est cupiditate enim
+                perspiciatis iusto pariatur dicta laudantium voluptatum.
+                Repellendus, incidunt facere blanditiis alias magnam tenetur
+                perspiciatis eum sapiente obcaecati facilis non. Natus magni
+                deleniti iusto excepturi odit sequi repellendus ratione deserunt
+                animi reiciendis beatae at possimus cum, blanditiis dignissimos
+                accusantium voluptatem ipsum, aspernatur id? Dignissimos
+                repellat vero ab culpa deserunt cum molestiae in maxime
+                doloribus, tempora, debitis velit eius? Modi asperiores optio
+                molestiae consectetur odio ea itaque, dolor dicta aspernatur,
+                laboriosam vitae in? Ipsa, dolorum officiis! Aut, vitae nulla.
+                Ad fuga, quo nisi, velit commodi sit, repellat illo minima
+                ducimus atque exercitationem quae iste voluptate officia quos?
+                Suscipit quam magnam ut quia aspernatur modi reiciendis.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    /* Rectangle 40 */
+    /**
+position: absolute;
+width: 965px;
+height: 2656px;
+left: 394px;
+top: 156px;
+
+background: #EDEDED;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 23px;
+ */
+  );
+};
+
+export default Experience;
