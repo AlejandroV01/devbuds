ac267687171bc8e14896209f3454cc6ccbf45b4d
Austinh1512 <Austinhouchens1510@gmail.com>
2024-03-16 04:41:15 -0400

    add optional className prop

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,38 @@
+interface ButtonProps {
+  variant: "primary" | "secondary" | "success" | "destructive";
+  disabled?: boolean;
+  onClick?: () => void;
+  className: string;
+  children: string;
+}
+
+const Button = ({
+  variant,
+  disabled = false,
+  onClick,
+  className,
+  children,
+}: ButtonProps) => {
+  const checkButtonVariance = () => {
+    switch (variant) {
+      case "primary":
+        return "bg-primary";
+      case "secondary":
+        return "bg-secondary";
+      case "destructive":
+        return "bg-destructive";
+    }
+  };
+
+  return (
+    <button
+      disabled={disabled}
+      className={`${checkButtonVariance()} px-3 py-1.5 text-white rounded font-medium w-fit ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
