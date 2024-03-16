54c3b510de0f45436ee928da120b7f722bfe8060
dab1an <dabianextras@gmail.com>
2024-03-16 01:39:15 -0400

    mistake :)

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -19,7 +19,7 @@ const Experience = ({
   description,
 }: ExperienceProps) => {
   return (
-    <div className=" w-full flex flex-col bg-white">
+    <div className=" w-full flex flex-col ">
       <div className="flex gap-4 h-[80px] mb-4 text-black">
         <img
           src={companyLogo}
@@ -44,19 +44,6 @@ const Experience = ({
         <p className="text-black">{description}</p>
       </div>
     </div>
-
-    /* Rectangle 40 */
-    /**
-position: absolute;
-width: 965px;
-height: 2656px;
-left: 394px;
-top: 156px;
-
-background: #EDEDED;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 23px;
- */
   );
 };
 
