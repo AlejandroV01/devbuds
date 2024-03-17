daee4a9049ccecd2df9a98c95354940832ba31aa
Alejandro Vera <alexvera0109@gmail.com>
2024-03-17 17:13:38 -0400

    state changes and minor styling change

--- client/src/components/Portfolio.tsx
+++ client/src/components/Portfolio.tsx
@@ -1,104 +1,92 @@
-import React from "react";
-import { IconType } from "react-icons";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { HiOutlinePaintBrush } from "react-icons/hi2";
-import { HiPencil } from "react-icons/hi2";
-import { useState } from "react";
-import { FaCheck } from "react-icons/fa6";
-
-interface PortofolioProps {
-  variant: string;
-  title?: string;
-  bgColor?: string;
-  domain?: string;
-  link?: string;
-  icon?: IconType;
+import React, { useState } from 'react'
+import { IconType } from 'react-icons'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaCheck } from 'react-icons/fa6'
+import { HiOutlinePaintBrush, HiPencil } from 'react-icons/hi2'
+interface PortfolioProps {
+  variant: string
+  title?: string
+  bgColor?: string
+  domain?: string
+  link?: string
+  icon?: IconType
+  handleStateLinkChange: (link: string, variant: string) => void
 }
 
-const Portfolio = ({
-  variant,
-  title,
-  bgColor,
-  domain,
-  link,
-  icon: Icon,
-}: PortofolioProps) => {
+const Portfolio = ({ variant, title, bgColor, domain, link, icon: Icon, handleStateLinkChange }: PortfolioProps) => {
   switch (variant) {
-    case "github":
-      title = "Github";
-      domain = "https://github.com/";
-      bgColor = "bg-[black]";
-      Icon = FaGithub;
+    case 'github':
+      title = 'Github'
+      domain = 'https://github.com/'
+      bgColor = 'bg-[black]'
+      Icon = FaGithub
 
-      break;
-    case "linkedin":
-      title = "LinkedIn";
-      domain = "https://linkedin.com/in/";
-      bgColor = "bg-[#0274B3]";
-      Icon = FaLinkedin;
-      break;
-    case "personal":
-      title = "Personal Website";
-      domain = "https://";
-      Icon = HiOutlinePaintBrush;
-      bgColor = "bg-primary";
-      break;
+      break
+    case 'linkedin':
+      title = 'LinkedIn'
+      domain = 'https://linkedin.com/in/'
+      bgColor = 'bg-[#0274B3]'
+      Icon = FaLinkedin
+      break
+    case 'personal':
+      title = 'Personal Website'
+      domain = 'https://'
+      Icon = HiOutlinePaintBrush
+      bgColor = 'bg-primary'
+      break
   }
 
-  const [userLink, setLink] = useState("");
-  const [input, setInput] = useState("");
-  const [showInput, setShowInput] = useState(false);
-  const [showEdit, setShowEdit] = useState(true);
+  const [userLink, setLink] = useState(link)
+  const [input, setInput] = useState('')
+  const [showInput, setShowInput] = useState(false)
+  const [showEdit, setShowEdit] = useState(true)
 
-  const handleLinkChange = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLink(input);
-    setShowInput(false);
-    setShowEdit(true);
-  };
+  const handleDBLinkChange = () => {
+    console.log('Change link in db to', input)
+    const dataBaseResult = true
+    if (dataBaseResult) {
+      handleStateLinkChange(input, variant)
+    }
+  }
+  const handleLinkChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    setLink(input)
+    handleDBLinkChange()
+    setShowInput(false)
+    setShowEdit(true)
+  }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
+    setInput(e.target.value)
+  }
 
   return (
-    <div className="w-full flex flex-col ">
-      <div className="flex items-center gap-4 h-[80px] mb-4 text-black ">
-        <div
-          className={`h-full rounded-[8px] w-[80px] drop-shad ${bgColor} flex justify-center items-center`}
-        >
-          {Icon && <Icon fontSize={"50px"} color="white" />}
+    <div className='w-full flex flex-col '>
+      <div className='flex items-center gap-4 h-[80px] mb-4 text-black '>
+        <div className={`h-full rounded-[8px] w-[80px] drop-shad ${bgColor} flex justify-center items-center`}>
+          {Icon && <Icon size={45} color='white' />}
         </div>
-        <div className="flex flex-col  ">
-          <span className="font-extrabold text-lg text-2">{title}</span>
+        <div className='flex flex-col  '>
+          <span className='font-extrabold text-lg text-2'>{title}</span>
 
-          <span className="flex items-center gap-2">
+          <span className='flex items-center gap-2'>
             <span>
-              <a
-                href={domain + userLink}
-                target="_blank"
-                className="hover:text-primary"
-              >
+              <a href={domain && domain + userLink} target='_blank' className='hover:text-primary'>
                 {domain}
                 {showEdit && <span>{userLink}</span>}
               </a>
             </span>
 
             {showInput && (
-              <form className="flex gap-2">
+              <form className='flex gap-2'>
                 <input
-                  type="text"
-                  className="outline outline-[#afafaf] outline-1 rounded-md pl-2"
-                  placeholder="johndoe"
+                  type='text'
+                  className='outline outline-[#afafaf] outline-1 rounded-md pl-2'
+                  placeholder='johndoe'
                   value={input}
                   onChange={handleInputChange}
                 />
-                <button
-                  type="submit"
-                  onClick={handleLinkChange}
-                  className="hover:text-primary"
-                >
+                <button type='submit' onClick={e => handleLinkChange(e)} className='hover:text-primary'>
                   <FaCheck />
                 </button>
               </form>
@@ -107,19 +95,19 @@ const Portfolio = ({
             {showEdit && (
               <button
                 onClick={() => {
-                  setShowInput(true);
-                  setShowEdit(false);
+                  setShowInput(true)
+                  setShowEdit(false)
                 }}
-                className="hover:text-primary"
+                className='hover:text-primary'
               >
-                <HiPencil color="#7B7B7B" />
+                <HiPencil color='#7B7B7B' />
               </button>
             )}
           </span>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
--- client/src/components/PortfolioCard.tsx
+++ client/src/components/PortfolioCard.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import Portfolio from "@/components/Portfolio";
-
+import Portfolio from '@/components/Portfolio'
+import { addProfile } from '@/store/auth/auth.slice'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
 const PortfolioCard = () => {
+  const user = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch()
+  const handleStateLinkChange = (link: string, variant: string) => {
+    if (variant === 'github') {
+      dispatch(addProfile({ ...user, github_link: link }))
+    } else if (variant === 'linkedin') {
+      dispatch(addProfile({ ...user, linkedin_link: link }))
+    } else if (variant === 'personal') {
+      dispatch(addProfile({ ...user, portfolio_link: link }))
+    }
+  }
   return (
-    <div className="w-[850px] py-5 px-5 pt-4 flex flex-col gap-7 rounded-[8px] drop-shad bg-[#E8E8E8]">
-      <div className="flex justify-between items-center">
-        <span className="font-extrabold text-2xl text-black">Portfolio</span>
+    <div className='w-full py-5 px-5 pt-4 flex flex-col gap-7 rounded-[8px] drop-shad bg-[#E8E8E8]'>
+      <div className='flex justify-between items-center'>
+        <span className='font-extrabold text-2xl text-black'>Portfolio</span>
       </div>
-      <div className="flex flex-col gap-7">
-        <Portfolio variant="github" />
-        <Portfolio variant="linkedin" />
-        <Portfolio variant="personal" />
+      <div className='flex flex-col gap-7'>
+        <Portfolio variant='github' link={user.github_link || ''} handleStateLinkChange={handleStateLinkChange} />
+        <Portfolio variant='linkedin' link={user.linkedin_link || ''} handleStateLinkChange={handleStateLinkChange} />
+        <Portfolio variant='personal' link={user.portfolio_link || ''} handleStateLinkChange={handleStateLinkChange} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PortfolioCard;
+export default PortfolioCard
