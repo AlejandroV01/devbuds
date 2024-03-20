7cc610416600c2d4f863ad1e9f28777c5c09afb9
Alejandro Vera <alexvera0109@gmail.com>
2024-03-20 16:55:37 -0400

    finished profile info card

--- client/src/components/ProfileInfoCard.tsx
+++ client/src/components/ProfileInfoCard.tsx
@@ -1,40 +1,42 @@
 import React from 'react'
-import { FaGraduationCap } from "react-icons/fa6";
-import { PiFlaskLight } from "react-icons/pi";
+import { FaGraduationCap } from 'react-icons/fa6'
+import { PiFlaskLight } from 'react-icons/pi'
 
 interface IProfileInfoCard {
-    userFirstName: string
-    userLastName: string
-    userSchool: string
-    userPhone: string
-    userEmail: string
-    userMajor: string
+  userFirstName: string
+  userLastName: string
+  userSchool: string
+  userPhone: string
+  userEmail: string
+  userMajor: string
 }
 
-const ProfileInfoCard = ({userFirstName, userLastName, userSchool, userPhone, userEmail, userMajor}: IProfileInfoCard) => {
+const ProfileInfoCard = ({ userFirstName, userLastName, userSchool, userPhone, userEmail, userMajor }: IProfileInfoCard) => {
   return (
-    <div className='content-center grid grid-cols-2 divide-x divide-accent w-[600px] h-[125px] bg-primary p-5 rounded-lg text-muted'>
-        <div className='flex-col'>
-            <div className='text-[18px] font-semibold'>
-                    {userFirstName} {userLastName}</div>
-            
-            <div className='text-[14px]'>
-                <div className='flex items-center'>
-                    <FaGraduationCap/> &nbsp; {userSchool}</div>
-                <div className='flex items-center'>
-                    <PiFlaskLight/> &nbsp; {userMajor}</div>
-            </div>
+    <div className='flex flex-col sm:flex-row gap-6 justify-between w-fit bg-primary p-5 rounded-lg text-white'>
+      <div className='flex-col'>
+        <div className='text-[18px] font-semibold'>
+          {userFirstName} {userLastName}
         </div>
-
-        <div className='flex-col px-7'>
-            <div className='text-[18px] font-semibold'>
-                Contact</div>
-            <div className='text-[14px]'>
-                {userEmail} <br/> {userPhone}</div>
+        <div className='text-[14px]'>
+          <div className='flex items-center gap-1'>
+            <FaGraduationCap />
+            <span>{userSchool}</span>
+          </div>
+          <div className='flex items-center gap-1'>
+            <PiFlaskLight />
+            <span>{userMajor}</span>
+          </div>
         </div>
-
+      </div>
+      <div className='w-[3px] bg-white/40' />
+      <div className='flex-col'>
+        <div className='text-[18px] font-semibold'>Contact</div>
+        <div className='text-[14px]'>
+          {userEmail} <br /> {userPhone}
+        </div>
+      </div>
     </div>
-    
   )
 }
 
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
 import ProfileInfoCard from '@/components/ProfileInfoCard'
+import React from 'react'
 //changed Badge file name
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <ProfileInfoCard
-      userFirstName={'Linda'}
-      userLastName={'Maali'}
-      userSchool={'Florida International University'}
-      userEmail={'linda@gmail.com'}
-      userPhone={'(123)-456-7890'}
-      userMajor={'Computer Science'}
-            />
+        userFirstName={'Linda'}
+        userLastName={'Maali'}
+        userSchool={'Florida International University'}
+        userEmail={'example@gmail.com'}
+        userPhone={'(123)-456-7890'}
+        userMajor={'Computer Science'}
+      />
     </div>
   )
 }
