a7ac593fa739b953f0e0ee556392c38a8310525f
dab1an <dabianextras@gmail.com>
2024-03-20 00:28:11 -0400

    Finished login component

--- client/src/components/LoginCard.tsx
+++ client/src/components/LoginCard.tsx
@@ -9,6 +9,7 @@ import { addProfileUuid } from "@/store/auth/auth.slice";
 const LoginCard = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [validCredentials, setValidCredentials] = useState(true);
 
   const currentProfile = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
@@ -21,7 +22,11 @@ const LoginCard = () => {
     });
     console.log(data, error);
     if (data.user) {
+      console.log("Successfully logged in!");
+      setValidCredentials(true);
       dispatch(addProfileUuid(data.user.id));
+    } else {
+      setValidCredentials(false);
     }
   };
 
@@ -33,13 +38,22 @@ const LoginCard = () => {
   return (
     <div className="h-screen w-full flex">
       <div className="h-full w-[45%] bg-white flex flex-col justify-center items-center ">
-        <div className="flex flex-col items-center justify-center gap-12">
+        <div className="flex flex-col items-center justify-center gap-5">
           <form onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-3 items-center justify-center ">
+            <div className="flex flex-col gap-2 items-center justify-center ">
               <h1 className="font-bold text-2xl">DevBuds</h1>
+
+              {!validCredentials && (
+                <span className=" w-full flex justify-start">
+                  <p className="text-xs text-destructive">
+                    Invalid login credentials.
+                  </p>
+                </span>
+              )}
+
               <div className="">
                 <label htmlFor="username" className="text-sm">
-                  Username
+                  Email
                 </label>
                 <Input
                   type="text"
@@ -54,7 +68,7 @@ const LoginCard = () => {
                   Password
                 </label>
                 <Input
-                  type="password" // Change type to password
+                  type="password"
                   value={userPassword}
                   onChange={(e) => setUserPassword(e.target.value)}
                   className="w-[325px]"
@@ -69,9 +83,17 @@ const LoginCard = () => {
               <Button variant="primary" children="Log In" className="w-full" />
             </div>
           </form>
+          <span className="">
+            <span className="text-sm">Don't have an account? </span>
+            <span className="text-sm text-primary underline cursor-pointer">
+              <a href="">Sign up</a>
+            </span>
+          </span>
         </div>
       </div>
-      <div className="h-full w-[55%] bg-primary"></div>
+      <div className="h-full w-[55%] bg-primary flex justify-center items-center">
+        <img src="/undraw_work_chat_re_qes4.svg" alt="" className="h-[350px]" />
+      </div>
     </div>
   );
 };
