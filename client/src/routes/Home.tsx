ee8056da5f1544506b21c1162f73c9912e5886bf
Alejandro Vera <alexvera0109@gmail.com>
2024-03-21 18:02:29 -0400

    Update Home.tsx return to normal

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,30 +1,10 @@
-import React, { useState } from 'react';
-
-const Home: React.FC = () => {
-  const [selectedValue, setSelectedValue] = useState<string | null>(null);
-
-  const handleChange = () => {
-    setSelectedValue(prevValue => prevValue === 'selected' ? null : 'selected');
-  };
-
+import React from 'react'
+const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <label className="inline-flex items-center">
-        <input
-          type="radio"
-          className="form-radio h-5 w-5 text-indigo-600"
-          checked={selectedValue === 'selected'}
-          onChange={handleChange}
-        />
-        <span className={`ml-2 ${selectedValue ? 'text-blue-500' : 'text-gray-700'}`}>
-          Option
-        </span>
-      </label>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
-
-
+export default Home
