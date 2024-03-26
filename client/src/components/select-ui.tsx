c479afe71f61553ca7feab53b0ee53e8b640fbee
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-3.local>
2024-03-26 15:26:55 -0400

    Added onChange to the select element seems to work for getting a return value.

--- client/src/components/select-ui.tsx
+++ client/src/components/select-ui.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import {Option} from '../types/index'
+import React, {useState} from 'react';
+import {Option} from '../types/index';
 
 export function SelectDemo({
   options,
   label,
-  onChange,
+  handleChange,
 }: {
-  label: string;
   options: Option[];
-  onChange: (selection: Option) => void;
+  label: string;
+  handleChange: (selection: Option) => void;
 }) {
   return (
-    <select name={label} id={label} className='p-2 bg-white shadow-[0_0_3.5px_1px_rgba(0,0,0,0.3)] rounded-lg w-[180px]]'>
+    <select name={label} id={label} onChange={(o) => console.log(o.target.value)} className='p-2 bg-white shadow-[0_0_3.5px_1px_rgba(0,0,0,0.3)] rounded-lg w-[180px]]'>
         <option value={label}>{label}</option>
         {
             options.map((listItem) => (
                 <option 
-                    value={listItem.value} 
-                    onClick={() => onChange(listItem)} 
+                    value={listItem.value}
+                    onClick={() => console.log(listItem)}
                     key={listItem.value} 
                 >
                     {listItem.label}
