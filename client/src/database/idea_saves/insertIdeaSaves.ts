48498a2f5cee325426d5bb345c1f2dd3b5262990
Joshua Galvez <manckijg@outlook.com>
2024-03-26 14:14:47 -0400

    Cleaned up Idea Saves code

--- client/src/database/idea_saves/insertIdeaSaves.ts
+++ client/src/database/idea_saves/insertIdeaSaves.ts
@@ -1,37 +1,20 @@
 import supabase from "@/lib/supabaseClient";
 
-const insertIdeaSaves = async (profile_id: number, idea_id: number): Promise<boolean> => {
-
+async function insertIdeaSaves(profile_id: number, idea_id: number): Promise<void> {
     try {
+        
+        const { data, error } = await supabase.from('idea_saves').insert([{ profile_id, idea_id }]);
 
-      const { data:ideas , error } = await supabase.from("idea_saves").insert([{ profile_id, idea_id }]);
-  
-//Errors:      
-      if (error) {
+        if (error) {
+            throw error;
+        }
 
-        console.error(error);
-        return false;
-
-      }
-  
-      if (ideas) {
-
-        console.log("Idea Saves successfully inserted:", ideas);
-        return true;
-
-      }
-  
-      return false;
-
+        console.log('Idea Saves sucessfully inserted:', data);
+        
     } 
-    
     catch (error) {
+      console.error('ERROR: Idea Saves not inserted', error);
+  }
+}
 
-      console.error("Unexpected error while inserting Idea Saves:", error);
-      return false;
-      
-    }
-
-  };
-  
-  export default insertIdeaSaves;
+export default insertIdeaSaves;
