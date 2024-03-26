61973e609a6adbd92f17c13decebbb5e1a306bf7
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-3.local>
2024-03-26 16:01:28 -0400

    Finished the select-ui component no errors looks good

--- client/src/components/select-ui.tsx
+++ client/src/components/select-ui.tsx
@@ -1,29 +1,22 @@
-import React, {useState} from 'react';
-import {Option} from '../types/index';
+import React from 'react'
 
-export function SelectDemo({
+export function Select({
   options,
   label,
-  onChange
+  onChange,
 }: {
-  options: Option[];
-  label: string;
-  onChange: (selection: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: string[]
+  label: string
+  onChange: (selection: React.ChangeEvent<HTMLSelectElement>) => void
 }) {
   return (
     <select name={label} id={label} onChange={onChange} className='p-2 bg-white shadow-[0_0_3.5px_1px_rgba(0,0,0,0.3)] rounded-lg w-[180px]]'>
-        <option value={label}>{label}</option>
-        {
-            options.map((listItem) => (
-                <option 
-                    value={listItem.value}
-                    onClick={() => console.log(listItem)}
-                    key={listItem.value} 
-                >
-                    {listItem.label}
-                </option>
-            ))
-        }
+      <option value={label}>{label}</option>
+      {options.map(listItem => (
+        <option value={listItem} key={listItem}>
+          {listItem}
+        </option>
+      ))}
     </select>
   )
 }
