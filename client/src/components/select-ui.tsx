f9f52cb1aa9340d39710ebcb776e944c8ce32b14
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-3.local>
2024-03-26 15:44:50 -0400

    Figured out how to use the .value property to return the value property to return by using a paragraph element to test the code

--- client/src/components/select-ui.tsx
+++ client/src/components/select-ui.tsx
@@ -4,14 +4,14 @@ import {Option} from '../types/index';
 export function SelectDemo({
   options,
   label,
-  handleChange,
+  onChange
 }: {
   options: Option[];
   label: string;
-  handleChange: (selection: Option) => void;
+  onChange: (selection: React.ChangeEvent<HTMLSelectElement>) => void;
 }) {
   return (
-    <select name={label} id={label} onChange={(o) => console.log(o.target.value)} className='p-2 bg-white shadow-[0_0_3.5px_1px_rgba(0,0,0,0.3)] rounded-lg w-[180px]]'>
+    <select name={label} id={label} onChange={onChange} className='p-2 bg-white shadow-[0_0_3.5px_1px_rgba(0,0,0,0.3)] rounded-lg w-[180px]]'>
         <option value={label}>{label}</option>
         {
             options.map((listItem) => (
