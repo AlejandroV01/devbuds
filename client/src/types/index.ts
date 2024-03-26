0c9c0344745960d999546cb24eb409b7d47a3eef
Alejandro Vera <alexvera0109@gmail.com>
2024-03-26 16:06:46 -0400

    Update index.ts

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,15 +1,4 @@
-export interface IIdeaTableTypes {
-  idea_id: number
-  idea_title: string
-  idea_description: string
-  tech_stack: string[]
-  front_end: number
-  back_end: number
-  full_stack: number
-  ux_ui: number
-}
-
-export interface IIdeaSavesTableTypes {
-  idea_id: number
-  profile_id: number
+export interface ICountries {
+  id: number
+  name: string
 }
