f2cb24152bcb1d9abdc4a71cb37597ab214ced79
Keren Rivera <kerenrivera1211@gmail.com>
2024-03-26 14:30:43 -0400

    Created updateIdeaSaves file

--- client/src/database/idea_saves/updateIdeaSaves.ts
+++ client/src/database/idea_saves/updateIdeaSaves.ts
@@ -1,12 +1,16 @@
 import supabase from "@/lib/supabaseClient";
-import { error } from "console";
+import { error} from "console";
 
 const updateIdeaSaves = async (newIdeaID: number, profile_id: number) => {
+    
     const { data, error } = await supabase
     .from('idea_saves')
-    .update( {idea_id: newIdeaID} )
+    .update(
+        {idea_id: newIdeaID}
+    )
     .eq('profile_id', profile_id)
     .select()
+
 
     if (data) {
         return data;
@@ -16,4 +20,4 @@ const updateIdeaSaves = async (newIdeaID: number, profile_id: number) => {
       }
 }
 
-export default updateIdeaSaves
+export default updateIdeaSaves;
