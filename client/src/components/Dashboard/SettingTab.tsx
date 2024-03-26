95e8b9a9120e1e07843ef8a87f70d432d7581da0
Alejandro Vera <alexvera0109@gmail.com>
2024-03-26 19:02:32 -0400

    added info to task

--- client/src/components/Dashboard/SettingTab.tsx
+++ client/src/components/Dashboard/SettingTab.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
-
+import fetchProfileByID from '@/database/profiles/fetchProfileByID'
+import { useAppSelector } from '@/store/hooks'
+import React, { useEffect, useState } from 'react'
+import Avatar from '../Avatar'
+import EducationCard from '../EducationCard'
+import ExperienceCard from '../ExperienceCard'
+import PortfolioCard from '../PortfolioCard'
+import ProfileInfoCard from '../ProfileInfoCard'
+import ProfileProjectCard from '../ProfileProjectCard'
 const SettingTab = () => {
+  const user = useAppSelector(state => state.auth)
+  console.log('user id: ', user.id)
+  /**
+   * Order of the components if the following
+   * 1. Avatar
+   * 2. ProfileInfoCard
+   * 3. ExperienceCard
+   * 4. EducationCard
+   * 5. ProfileProjectCard
+   * 6. PortfolioCard
+   * Use the fetchProfileByID function to get the user's profile
+   * I made it so that in this branch the user.id is default to 1 which is
+   * a valid user id in the database
+   * Do not worry about the Skills Card, it is still in development
+   * If you have any questions, please ask!
+   */
   return (
     <div className='w-full bg-violet-500 h-[2000px]'>
       <h1>Setting Tab</h1>
