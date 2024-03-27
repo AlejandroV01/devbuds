3265232217d2beca84b83aef3263806d52862492
Alejandro Vera <alexvera0109@gmail.com>
2024-03-26 20:48:06 -0400

    changes from 1on1

--- client/src/components/SkillCard.tsx
+++ client/src/components/SkillCard.tsx
@@ -1,54 +1,57 @@
 import React, { useState } from 'react'
+import { CiEdit } from 'react-icons/ci'
+import { FaXmark } from 'react-icons/fa6'
+import { RiPencilFill } from 'react-icons/ri'
 import Button from './Button'
-import Badge from './badge'
 import Input from './Input'
-import { CiEdit } from "react-icons/ci";
-
-
-
-
-const SkillCard = ({ onFinishEditing }: { onFinishEditing: () => void }) => {
+import Badge from './badge'
+const SkillCard = ({
+  onFinishEditing,
+  skills,
+  onDoneClick,
+}: {
+  onFinishEditing: () => void
+  skills: string[]
+  onDoneClick: (e: React.FormEvent<HTMLFormElement>, val: string) => void
+}) => {
   const [inputSkill, setInputSkill] = useState<string>('')
-  const [inventory, setInventory] = useState<{ name: string} []>([]);
+  const [editMode, setEditMode] = useState<boolean>(false)
   const handleAddSkill = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (!inputSkill) return;
-    setInventory(prevInventory => [...prevInventory, {name: inputSkill}]);
-    setInputSkill('');
+    event.preventDefault()
+    if (!inputSkill) return
+    setInputSkill('')
   }
   const handleEdit = (event: React.FormEvent) => {
-    event.preventDefault();
-    
+    event.preventDefault()
   }
- 
+
   return (
-    <div className='flex flex-col items-center gap-4 bg-blue-400'>
-      <h1 className='text-center font-bold text-3xl'>Skills Section</h1>
-      <form onSubmit= {handleAddSkill} className = 'flex gap-3'>
-        <input 
-        type= 'text'
-        value = {inputSkill}
-        onChange={event => setInputSkill(event.target.value)}
-        className = 'input'
-        />
-        <Button variant='secondary'>Done</Button>
-        <CiEdit className='gap-3 size-8' />
-      </form>
-      <div className ='text-center p-3 rounded-lg flex justify-center '>
-      {inventory.map((skillObject, index) => (
-          <SkillBox key={index} skill={skillObject.name} />
+    <div className='flex flex-col gap-4 bg-blue-400 p-6'>
+      <div className='flex items-center justify-between'>
+        <h1 className='font-bold text-3xl'>Skills</h1>
+        <RiPencilFill size={18} />
+        <FaXmark size={18} />
+      </div>
+      {/**
+       * Edit mode will change decide if the div underneath this comment is shown
+       * Edit mode will also decide which icon is shown like the pencil
+       * onClick of x should also call the same function as the Finish button
+       */}
+      <div className='flex gap-5'>
+        <form onSubmit={e => onDoneClick(e, inputSkill)} className='flex gap-3'>
+          <Input type='text' value={inputSkill} onChange={event => setInputSkill(event.target.value)} className='bg-white' />
+          <Button variant='primary'>Enter Skill</Button>
+        </form>
+        <Button variant='secondary' onClick={onFinishEditing}>
+          Finish
+        </Button>
+      </div>
+      <div className='flex justify-start gap-2 flex-wrap'>
+        {skills.map((skill, index) => (
+          <Badge label={skill} key={index} />
         ))}
       </div>
     </div>
   )
 }
-const SkillBox = ({skill}: {skill: string}) => {
-  return (
-    <div className='text-center items-center p-2'>
-      <li>{skill}</li>
-    </div>
-  )
-}
-
-
-export default SkillCard;
+export default SkillCard
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 //changed Badge file name
 import SkillCard from '@/components/SkillCard'
 const Home = () => {
+  const [profileSkills, setProfileSkills] = useState(['React', 'TypeScript'])
+
+  const changeProfileSkills = (event: React.FormEvent<HTMLFormElement>, newSkill: string) => {
+    event.preventDefault()
+    setProfileSkills([...profileSkills, newSkill])
+  }
   return (
     <div>
       <h1>Home</h1>
-      <SkillCard />
+      <SkillCard skills={profileSkills} onDoneClick={changeProfileSkills} />
     </div>
   )
 }
