d09b09b1b68b6fe802a584e07663a5eb3b6dd499
dab1an <dabianextras@gmail.com>
2024-03-28 16:06:01 -0400

    settings tab rendering from dashboard

--- client/src/components/Dashboard/SettingsTab.tsx
+++ client/src/components/Dashboard/SettingsTab.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { lazy } from "react";
 import SettingsInput from "@/components/SettingsInput";
 const SettingsTab = () => {
+  const handleSave = (firstName: string, lastName: string, email: string) => {
+    firstName = "John";
+    lastName = "Doe";
+    email = "alexvera0109@gmail.com";
+  };
+
   return (
     <div>
       {" "}
-      <SettingsInput />
+      <SettingsInput handleSave={handleSave} />
     </div>
   );
 };
